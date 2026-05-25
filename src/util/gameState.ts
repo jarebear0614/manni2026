476e464729585dc88b69feb13376fa3517a1b7ae
Arc1llusion <arc1llusion@gmail.com>
2026-05-25 17:04:47 -0400

    More build stuff

--- src/util/gameState.ts
+++ src/util/gameState.ts
@@ -6,20 +6,5 @@ export class GameState
 
     spawnY?: number = 36;
 
-    completedConstellations: boolean = false;
-
     fromScene?: string = '';
-
-    birdwingSpawn: {x: number, y: number} | undefined = undefined; 
-    hairStreakSpawn: {x: number, y: number} | undefined = undefined;
-    lunaMothSpawn: {x: number, y: number} | undefined = undefined;
-    perianderSpawn: {x: number, y: number} | undefined = undefined;
-
-    birdwingButterflyObtained: boolean = false;
-
-    hairStreakButterflyObtained: boolean = false;
-
-    lunaMothButterflyObtained: boolean = false;
-
-    perianderButterflyObtained: boolean = false;
 }
